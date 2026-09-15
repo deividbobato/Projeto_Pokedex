698cc7c56d0c09375102eb1bc8001e0a0053e948
Joao Vinicius <joaoviniciusmoba01@gmail.com>
2026-09-15 20:38:44 -0300

    Adiciona estrutura JSON  do mew

---
+++ src/script.js
@@ -0,0 +1,24 @@
+
+const listaPokemons = [ 
+    {
+    "id": 151,
+    "name": "mew",
+    "types": ["Psychic"],
+    "height": 0.4,
+    "weight": 4.0,
+    "category": "New Species",
+    "abilities": ["Synchronize"],
+    "weakness": ["Bug", "Ghost", "Dark"],
+    "resistances": [],
+    
+    "stats": {
+        "hp": 100,
+        "attack": 100,
+        "defense": 100,
+        "specialAttack": 100,
+        "specialDefense": 100,
+        "speed": 100,
+        "total": 600
+    }
+}
+]
