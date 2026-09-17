6179c7092d71216a1d99098d68ef72f15f6ee261
Deivid Bobato <bobato.dede@gmail.com>
2026-09-17 17:42:46 -0300

    Unifica array JSON com os 3 pokemons

--- src/script.js
+++ src/script.js
@@ -1,26 +1,65 @@
 // array que recebe os JSON da API para os pokemons
 const listaPokemons = [
-    {"id": 4,
-    "name" : "charmander",
-    "types" : ["Fire"],
-    "height" : 0.6,     
-    "weight" : 8.5,
-    "category": "Lizard",
-    "abilities" : ["Blaze"],
-    "weakness": ["Water", "Ground", "Rock"],
-    "resistances" : ["Grass", "Bug", "Fairy"],
-    
-    "stats" : {
-        "hp" : 39,
-        "attack" : 52,
-        "defense" : 43,
-        "specialAttack" : 60,
-        "specialDefense" : 50,
-        "speed": 65,    
-        "total": 309
-     }
-    }  
- ];
+    {
+        "id": 384,
+        "name": "rayquaza",
+        "types": ["Dragon", "Flying"],
+        "height": 7.0,
+        "weight": 206.5,
+        "category": "Sky High",
+        "abilities": ["Air Lock"],
+        "weakness": ["Ice", "Rock", "Dragon", "Fairy"],
+        "resistances": ["Fire", "Water", "Grass", "Fighting", "Bug"],
+        "stats": {
+            "hp": 105,
+            "attack": 150,
+            "defense": 90,
+            "specialAttack": 150,
+            "specialDefense": 90,
+            "speed": 95,
+            "total": 680
+        }
+    }, //  separando o Rayquaza do Mew
+    {
+        "id": 151,
+        "name": "mew",
+        "types": ["Psychic"],
+        "height": 0.4,
+        "weight": 4.0,
+        "category": "New Species",
+        "abilities": ["Synchronize"],
+        "weakness": ["Bug", "Ghost", "Dark"],
+        "resistances": [],
+        "stats": {
+            "hp": 100,
+            "attack": 100,
+            "defense": 100,
+            "specialAttack": 100,
+            "specialDefense": 100,
+            "speed": 100,
+            "total": 600
+        }
+    }, // separando o Mew do Charmander
+    {
+        "id": 4,
+        "name": "charmander",
+        "types": ["Fire"],
+        "height": 0.6,
+        "weight": 8.5,
+        "category": "Lizard",
+        "abilities": ["Blaze"],
+        "weakness": ["Water", "Ground", "Rock"],
+        "resistances": ["Grass", "Bug", "Fairy"],
+        "stats": {
+            "hp": 39,
+            "attack": 52,
+            "defense": 43,
+            "specialAttack": 60,
+            "specialDefense": 50,
+            "speed": 65,
+            "total": 309
+        }
+    }
+];
 
- console.log(listaPokemons);
-    //em JSON, usamos pares de CHAVE e VALOR - CHAVE do lado esquerdo entre aspas duplas VALOR do lado direito dos dois pontos 
+console.log(listaPokemons);
